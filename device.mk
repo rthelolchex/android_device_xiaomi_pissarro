95c783e1191ebaef7f74cedbed992f85b4256b37
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    pissarro: Build only system HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -87,8 +87,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0_system
 
 # Init
 PRODUCT_COPY_FILES += \

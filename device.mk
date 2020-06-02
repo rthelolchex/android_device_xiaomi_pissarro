4c6f9ef0b56c985711e8de209f458af9f2a0d6f0
Kshitij Gupta <kshitijgm@gmail.com>
2020-06-03 00:40:07 +0530

    pissarro: Add DT2W service
    Change-Id: Ia503b95831e138bb4f8aec5762b9f637f6d2451b

--- device.mk
+++ device.mk
@@ -94,6 +94,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.renderengine.backend=skiaglthreaded
 
+# DT2W
+PRODUCT_PACKAGES += \
+    DT2W-Service-MT6877
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
